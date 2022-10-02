6c8dd3c810f7ff51d28729a902a108cf508992fd
Sanglim <54923245+sanglim00@users.noreply.github.com>
2022-10-03 02:07:47 +0900

    Update fastFibonacci.cpp

--- fastFibonacci.cpp
+++ fastFibonacci.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
 using namespace std;
 
-int *arr;
+void mul(int fibo[2][2], int matrix[2][2]){
+    int leftTop, rightTop, leftBottom, rightBottom;
+
+    leftTop =  fibo[0][0] * matrix[0][0] + fibo[0][1] * matrix[1][0];
+    rightTop =  fibo[0][0] * matrix[0][1] + fibo[0][1] * matrix[1][1];
+    leftBottom =  fibo[1][0] * matrix[0][0] + fibo[1][1] * matrix[1][0];
+    rightBottom =  fibo[1][0] * matrix[0][1] + fibo[1][1] * matrix[1][1];
+
+    fibo[0][0] = leftTop % 1000;
+    fibo[0][1] = rightTop % 1000;
+    fibo[1][0] = leftBottom % 1000;
+    fibo[1][1] = rightBottom % 1000;
+};
+
+void pow(int fibo[2][2], int num){
+    int matrix[2][2] = {{1, 1}, {1, 0}};
+    
+    if (num > 1) {
+        pow(fibo, num/2);
+        mul(fibo, fibo);
+
+        if (num % 2 != 0) mul(fibo, matrix);
+    }
+};
 
 int fastFibonacci(int num) {
+    int fibo[2][2] = {{1, 1}, {1, 0}};
 
-    int k, a, b;
-    if(num <= 1) {
-        arr[num] = num;
-        return num;
-    }
-    if(arr[num]) return arr[num];
+    if (num <= 1) return num;
+    pow(fibo, num-1);
 
-    if(num & 1) k = (num + 1)/2;
-    else k = num/2;
-
-    a = fastFibonacci(k) % 1000;
-    b = fastFibonacci(k-1) % 1000;
-
-    if(num & 1) arr[num] = (a * a + b * b)%1000;
-    else arr[num] = ((2 * b + a)*a)%1000;
-
-    return arr[num] % 1000;
+    return fibo[0][0];
 }
 
 int main() {
 
     int times, num;
 
-    cin>> times;
+    cin >> times;
     for(int i = 0; i< times; i++) {
         cin >> num;
-        arr = new int[num];
         int answer = fastFibonacci(num);
         cout<< answer<< endl;
-        
     }
 
     return 0;
