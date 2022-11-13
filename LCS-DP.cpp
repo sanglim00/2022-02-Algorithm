f91286dc1e34d8dc9e43110bde689f70870e156c
Sanglim <54923245+sanglim00@users.noreply.github.com>
2022-11-13 20:20:49 +0900

    Update LCS-DP.cpp

--- LCS-DP.cpp
+++ LCS-DP.cpp
@@ -1,18 +1,19 @@
-
 #include <iostream>
 using namespace std;
 
 #define MAX 100
+#define max(a, b) (a > b) ? a : b 
 
 int L[MAX][MAX], S[MAX][MAX];
+string C[MAX][MAX];
 
-int lengthLCS(string str, string str2, int m, int n){
+void lengthLCS(string str, string str2, int m, int n){
     
     for(int i=0; i<=m; i++) L[i][0] = 0;
     for(int i=0; i<=n; i++) L[0][i] = 0;
 
-    for(int i=1; i<=m; i++) {
-        for(int j=1; j<=n; j++){
+    for(int i=1; i <= m; i++) {
+        for(int j=1; j <= n; j++){
             if(str[i-1] == str2[j-1]) {
                 L[i][j] = L[i-1][j-1] + 1;
                 S[i][j] = 0;
@@ -28,21 +29,35 @@ int lengthLCS(string str, string str2, int m, int n){
             }
         }
     }
-    return L[m][n];
+    cout<<L[m][n]<<" ";
+    
+}
+
+void printLCS(string str, string str2, int m, int n) {
+    if(m == 0 || n == 0) return ;
+
+    if(S[m][n] == 0) {
+        printLCS(str, str2, m-1, n-1);
+        cout<< str[m-1];
+    }
+    else if(S[m][n] ==1) printLCS(str, str2, m, n-1);
+    else  printLCS(str, str2, m-1, n);
 }
 
 
 int main() {
 
-    int times;
+    int times, m, n;
     string str, str2;
 
     cin>> times;
     for(int i = 0; i< times; i++) {
-        cin>> str>>str2;
-        int answer = lengthLCS(str, str2, str.length(), str2.length());
-        cout<<answer<<endl;
-        
+        cin>> str >> str2;
+        m = str.length();
+        n = str2.length();
+        lengthLCS(str, str2, m, n);   
+        printLCS(str, str2, m, n);
+        cout<<endl;     
     }
     return 0;
 }
