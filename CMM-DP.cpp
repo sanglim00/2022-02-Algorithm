8e79846708db9926276605eb04c267bebef9bdfa
Sanglim <54923245+sanglim00@users.noreply.github.com>
2022-11-13 23:10:17 +0900

    Update CMM-DP.cpp

--- CMM-DP.cpp
+++ CMM-DP.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
+#define MAX 101
+
+int mat[MAX][MAX], P[MAX][MAX];
 
 int CMM(int arr[], int num) {
-    int** mat = new int*[num];
- 
-    for (int i = 0; i < num; i++) mat[i] = new int[num];
-    for (int i = 1; i < num; i++) mat[i][i] = 0;
+   
+    for (int i = 0; i < num; i++) mat[i][i] = 0;
  
     for (int diagonal = 1; diagonal < num; diagonal++) {
         for (int j = 1; j < num - diagonal; j++) {
@@ -18,12 +19,24 @@ int CMM(int arr[], int num) {
                 int sum = mat[j][L] + mat[L + 1][k] + arr[j - 1] * arr[L] * arr[k];
                 if (sum < mat[j][k]) {
                     mat[j][k] = sum;
+                    P[j][k] = L;
                 }
             }
         }
     }
-
     return mat[1][num - 1];
+}
+
+void printOrder(int i, int j) {
+    if(i == j) 
+        cout<< "M" << i;
+    else {
+        int k = P[i][j];
+        cout<<"(";
+        printOrder(i, k);
+        printOrder(k + 1, j);
+        cout<<")";
+    }
 }
 
  
@@ -40,8 +53,10 @@ int main(void) {
             cin >> data;
             arr[j] = data;
         }
+
         int answer = CMM(arr, num+1);
-        cout<<answer<<endl;
+        printOrder(1, num);
+        cout<<endl<<answer<<endl;
     }
 
     return 0;
